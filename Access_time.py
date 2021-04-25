bc1e35cad83169df62d008d87eb1c2a8e2695181
dvleeuwen99 <dvleeuwen.99@gmail.com>
2021-04-25 21:33:39 +0200

    implemented the valuefunction

--- Access_time.py
+++ Access_time.py
@@ -19,6 +19,7 @@ NrAppointments_List = data['Number of appointment requests'].values.tolist() #li
 Regular_List = data['Regular week'].values.tolist()
 Holiday_List = data['Holiday week'].values.tolist()
 
+
 E_Reg = 0
 E_Hol = 0
 for i in range(len(NrAppointments_List)):
@@ -26,16 +27,28 @@ for i in range(len(NrAppointments_List)):
     E_Hol += NrAppointments_List[i]*Holiday_List[i] #the expected number of appointment requests in Holiday week
 # print(E_Reg)
 # print(E_Hol)
+Cu=0.1
+Cp=0.1
+E=np.zeros(shape=(31,11))
+for i in range(31):
+    for j in range(11):
+        E[i,j] = (sum(NrAppointments_List[k-24]*Regular_List[k-24]*Cu*(12*j-i-k) for k in range(max(0,24),min(12*j-i+1,76)))
+            +sum(NrAppointments_List[k-24]*Regular_List[k-24]*Cp*(i+k-12*j) for k in range(max(24,12*j-i),min(120-i,76))))
+
 
 
 def valuefunction(n):
-    V=np.zeros(shape=(n+1,31))
+    V=np.zeros(shape=(n+1,1000))
     for i in range(31):
-        V[4, i] = 2
-    for k in range(n - 1, 0, -1):
+        V[n, i] = np.amin(E[i,])
+        print(n,i,V[n,i])
+    for m in range(n - 1, 0, -1):
         for i in range(29,-1,-1):
-            V[k,i] = min(k*(i+1),(k+i)*V[k+1,i])
-            print(k,i,V[k,i])
+                V[m,i] = min(E[i,j]
+                    +sum(NrAppointments_List[k-24]*Regular_List[k-24]*V[m+1,0] for k in range(24,min(12*j-i,76)))
+                    +sum(NrAppointments_List[k-24]*Regular_List[k-24]*V[m+1,i+k-12*j] for k in range(max(24,12*j-i),min(76,120-i)))
+                        for j in range(11))
+                print(m,i,V[m,i])
     return V[1,0]
 
 print(valuefunction(4))
